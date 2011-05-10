dd28bc0f10499f3a4b2544da49de4b74648dd33a
chrisjones0099 <chrisjones0099@gmail.com>
2011-05-10 08:24:06 +0000

    imported from codeplex

---
+++ Hippo/Assets/Resources/Actor/Hippo/Behaviours/HeadAction.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class HeadAction : MonoBehaviour {
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision");
+
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+
+    }
+}
